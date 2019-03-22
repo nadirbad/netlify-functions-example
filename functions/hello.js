b2334d77431cee0e31399e7968d383587b7f7f28
Nadir Badnjevic <nadirbad@gmail.com>
2019-03-22 22:02:45 +0100

    add hello function

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,7 @@
 exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: `Hello ${subject}!`,
-    }
-  }
+  const subject = event.queryStringParameters.name || 'World';
+  return {
+    statusCode: 200,
+    body: `Hello ${subject}!`
+  };
+};
